022398da57b8d920e8614922459fee241248a203
liuyu04 <liuyu04@qianxin.com>
2021-09-22 15:33:59 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rollup-md2vue
-组件库展示，md文件转为vue文件，将其中demo分为组件渲染和代码展示两部分
+
+组件库展示，md 文件转为 vue 文件，将其中 demo 分为组件渲染和代码展示两部分
+
+![img](https://raw.githubusercontent.com/liuyu666/picture/master/md-img/md2vue.png)
---
+++ src/container.js
@@ -0,0 +1,26 @@
+// 把md中的demo部分取出来 分成两份，一份代码高亮展示，一份渲染
+const mdContainer = require('markdown-it-container')
+
+module.exports = (md) => {
+  md.use(mdContainer, 'demo', {
+    validate (params) {
+      return params.trim().match(/^demo\s*(.*)$/)
+    },
+    render (tokens, idx) {
+      const m = tokens[idx].info.trim().match(/^demo\s*(.*)$/)
+      if (tokens[idx].nesting === 1) {
+        const description = m && m.length > 1 ? m[1] : ''
+        const content =
+          tokens[idx + 1].type === 'fence' ? tokens[idx + 1].content : ''
+        return `<demo-block>
+        ${description ? `<div>${md.render(description)}</div>` : ''}
+        <!--element-demo: ${content}:element-demo-->
+        `
+      }
+      return '</demo-block>'
+    }
+  })
+
+  md.use(mdContainer, 'tip')
+  md.use(mdContainer, 'warning')
+}
